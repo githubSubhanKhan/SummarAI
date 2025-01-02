1d790a7e68683ee526a410782b7b8ce56bbf554b
Muhammad Subhan Khan <subbikhan124@gmail.com>
2025-01-03 00:36:07 +0500

    added signup card

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -3,12 +3,22 @@ import { Link } from 'react-router-dom';
 
 const Signup = () => {
   return (
-    <div>
-      <h1>Signup</h1>
-      <Link to="/login">
-        <button className="btn btn-primary">Create Account</button>
-      </Link>
-    </div>
+    <div className="container my-5">
+            <div className="card" style={{ width: "18rem" }}>
+                <img src="" className="card-img-top" alt="" />
+                <div className="card-body">
+                    <div class="mb-3">
+                        <label for="username" class="form-label">Username</label>
+                        <input type="text" class="form-control" id="username"/>
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="password"/>
+                    </div>
+                    <Link to="/login" className="btn btn-primary">Create Account</Link>
+                </div>
+            </div>
+        </div>
   );
 }
 
