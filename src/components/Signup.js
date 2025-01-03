eb4218d9009174e93f910c3307a17654033b34fd
Muhammad Subhan Khan <subbikhan124@gmail.com>
2025-01-03 12:40:06 +0500

    added required fields

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -4,22 +4,29 @@ import ImageUpload from './ImageUpload';
 
 const Signup = () => {
   return (
-    <div className="container my-5">
+    <div className="container d-flex justify-content-center align-items-center my-5">
       <div className="card" style={{ width: "18rem" }}>
         <div className="container d-flex justify-content-center mt-5">
           <ImageUpload/>
         </div>
 
         <div className="card-body">
+          <form>
+
           <div className="mb-3">
             <label htmlFor="username" className="form-label">Username</label>
-            <input type="text" className="form-control" id="username" />
+            <input type="text" className="form-control" id="username" required/>
           </div>
           <div className="mb-3">
             <label htmlFor="password" className="form-label">Password</label>
-            <input type="password" className="form-control" id="password" />
+            <input type="password" className="form-control" id="password" required/>
           </div>
-          <Link to="/login" className="btn btn-primary">Create Account</Link>
+          <div className="mb-3">
+            <label htmlFor="confirmPassword" className="form-label">Confirm Password</label>
+            <input type="password" className="form-control" id="confirmPassword" required/>
+          </div>
+          <button type='submit' className="btn btn-primary">Create Account</button>
+          </form>
         </div>
       </div>
     </div>
