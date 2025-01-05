f55713f95510297fde536c09d288135e4b431fcc
Muhammad Subhan Khan <subbikhan124@gmail.com>
2025-01-05 12:30:01 +0500

    fixed a tag in comments

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -3,14 +3,12 @@ import React from 'react';
 const Comments = () => {
     return (
         <div>
-            <div class="list-group w-100">
-                <a href="#" class="list-group-item list-group-item-action">
+            <div class="list-group w-100 list-group-item list-group-item-action">
                     <div class="d-flex w-100 justify-content-between">
                         <h5 class="mb-1">Abbas</h5>
                         <small>3 days ago</small>
                     </div>
                     <p class="mb-1">Hello World</p>
-                </a>
             </div>
         </div>
     );
