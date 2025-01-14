046eddaaa285dbee5e9810382954cf4b320239a3
Muhammad Subhan Khan <subbikhan124@gmail.com>
2025-01-14 22:10:37 +0500

    updated .env code

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const connectToMongo = require('./config/db'); // Adjust path to your db.js file
 const cors = require('cors');
-require('dotenv').config();
+require('dotenv').config({ path: '../.env' });
 
 const app = express();
 
